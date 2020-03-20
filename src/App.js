d5a391c6b8d191346a7bf4d4293540d775e06fc5
consuelowevar <consue_1990@hotmail.com>
2020-03-20 11:13:07 -0300

    conectando app con mapa base inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+//App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {DisplayMapClass} from './DisplayMapClass';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Esta es nuestra aplicación para el coronavirus
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<DisplayMapClass />
+);
 }
-
 export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Esta es nuestra aplicación para el coronavirus
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
