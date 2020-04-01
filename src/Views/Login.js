3297fe191b0d8eb0b3c8eba4d1a7d56f9f986cf0
SusejR <rodriguezbsusej@gmail.com>
2020-04-01 08:39:12 -0300

    agregando materialize y firebase

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import firebase from 'firebase';
 
 class Login extends Component {
+handleAuth () {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+  .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+  .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+}
     render() {
       return (
         <div className="section">
@@ -12,7 +20,8 @@ class Login extends Component {
             <input id="inputUserName" className="inputText" placeholder="Usuario"></input>
             <input id="inputPassword" className="inputText" placeholder="Password"></input>
             <Link to="/Home">
-            <button className="button">Ingresar</button>
+            <button>Iniciar Sesion</button>
+            <button onClick={this.handleAuth}>Login con Google</button>
             </Link>
           </form>
         </div>
