45cd9ed413ff3860b5818cf9cc39059a2326b9f6
SusejR <rodriguezbsusej@gmail.com>
2020-04-01 17:54:22 -0300

    agregando estilos a pagina de inicio

--- src/Views/Welcome.js
+++ src/Views/Welcome.js
@@ -7,18 +7,25 @@ import './welcome.css'
 class Welcome extends Component {
   render() {
     return (
-      <div id="BackgroundColor"
-        className="section container">
-        <h1>Bienvenido a App Salud</h1>
-        <Link to="/RegisterDoc"> 
-        <button className="button">Soy Profesional de Salud</button>
-        </Link> 
-
-        <button className="button">Soy Paciente</button>
+      <div className="row section container">
         
+
+      
+      <div className="card light-green lighten-5">
+        <div className="card-content center">
+        <h1 className="col s12 m12 l12">Bienvenido a App Salud</h1>
+        <Link to="/RegisterDoc"> 
+        <button className="btn black-text   light-green lighten-3 waves-effect  s12 m6 l6">Soy Profesional de Salud</button>
+        </Link> 
+        <button className="btn black-text light-green lighten-3 waves-effect waves-light s12 m6 l6">Soy Paciente</button>
+        <div className="link s12 m12 l12">
         <Link to="/Login"> 
         <a href="">Ya estoy registrado</a>
         </Link>
+        </div>
+        
+        </div>
+      </div>
       </div>
     );
   }
